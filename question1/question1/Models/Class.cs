7a34e15f38b81c96cc04e0192a21036f00fc2367
Kett <saintmorose@gmail.com>
2020-01-30 12:48:27 -0700

    CreatedProject

---
+++ question1/question1/Models/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace question1.Models
+{
+    public class Department
+    {
+        public long id;             //may be unnecessary with unique address
+        public string name;
+        public string address;
+    }
+
+    public class Employee
+    {
+        public long id;
+        public string firstName;
+        public string lastName;
+        public string jobTitle;
+        public string residenceAddress;
+    }
+}
